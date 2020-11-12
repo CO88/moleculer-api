cfd8c873764043aa2c641aa46ed59d7583af2eaf
cobb <121212112@naver.com>
2020-11-12 23:06:01 +0900

    서비스로드부분 추가중

--- src/moleculer.broker.ts
+++ src/moleculer.broker.ts
@@ -2,6 +2,18 @@ import brokerConfig from "./moleculer.config";
 import { ServiceBroker } from "moleculer";
 
 const broker = new ServiceBroker(brokerConfig);
-broker.loadServices("./src/services/**", "*service.ts");
+
+/**
+ * 서비스를 로드합니다.
+ * 1. SERVICES env var가 있으면 해당 서비스를 로드합니다.
+ *    SERVICEDIR을 체크합니다.
+ * 2. SERVICES env var가 없으면 모든 서비스를 로드합니다.
+ */
+const fileMask = "*.service.ts";
+
+
+broker.loadServices("./src/services/**", fileMask);
+
+
 
 export = broker
